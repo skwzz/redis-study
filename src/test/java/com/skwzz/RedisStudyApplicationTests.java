a860a1e730d3b68e0e70a984bbdd18e11a96200e
skwzz <kidol9108@gmail.com>
2024-03-31 18:21:53 +0900

    Initial commit

---
+++ src/test/java/com/skwzz/RedisStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
